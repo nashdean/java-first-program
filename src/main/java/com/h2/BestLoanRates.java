f6033cbe93bfec8b343ff2950b7e65a36511a6d0
nashdean <nashtdean@gmail.com>
2021-12-22 13:59:31 -0500

    Update BestLoanRates.java

--- src/main/java/com/h2/BestLoanRates.java
+++ src/main/java/com/h2/BestLoanRates.java
@@ -15,7 +15,7 @@ public class BestLoanRates {
     public static void main(String[] args){
         try(Scanner scanner = new Scanner(System.in);) {
 
-            System.out.println("Enter your name.");
+            System.out.println("Enter your name");
             String name = scanner.nextLine();
 
             System.out.println("Hello " + name);
